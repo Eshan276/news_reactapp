a55da8a8b036ce0ea0672aca1ebf5ad3d22e700b
Eshan276 <eshandas2002@gmail.com>
2024-03-14 20:34:51 +0530

    updated css

--- newsapi_ft/newsapp/src/newss.jsx
+++ newsapi_ft/newsapp/src/newss.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./NewsComponent.css"; // Import the CSS file
 
 const NewsComponent = ({ category }) => {
   const [news, setNews] = useState([]);
