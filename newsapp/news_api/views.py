a880c551fcc8e3ce79e4a9f5932374bf85969ba0
Eshan276 <eshandas2002@gmail.com>
2024-03-14 20:33:01 +0530

    fixed frontend css

--- newsapp/news_api/views.py
+++ newsapp/news_api/views.py
@@ -4,14 +4,16 @@ from django.http import JsonResponse
 def home(request):
     category=request.GET.get('category')
     country='in'
+    #you can change country code
     api_key = '25d2fd9ddb614f5eaa45ffd81c4c4e6a'
+    #You can replace with your own api key
     if (category):
         if(category=='latest news'):
-            api_url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey=25d2fd9ddb614f5eaa45ffd81c4c4e6a'
+            api_url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}'
         else:
-            api_url = f'https://newsapi.org/v2/top-headlines?country={country}&category={category}&apiKey=25d2fd9ddb614f5eaa45ffd81c4c4e6a'
+            api_url = f'https://newsapi.org/v2/top-headlines?country={country}&category={category}&apiKey={api_key}'
     else:
-        api_url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey=25d2fd9ddb614f5eaa45ffd81c4c4e6a'
+        api_url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}'
     
     try:
         response = requests.get(api_url)
